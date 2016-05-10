50ee9beeb3913e2486bd693f79ec5b4e3ccf8228
Macklin Underdown <macklin@detroitlabs.com>
2016-05-10 15:35:08 -0400

    Add redux-slider-monitor typedef stub
    This commit adds the type definition for redux-slider-monitor.
    There are currently no defined properties; these are TODO.
    
    e Please enter the commit message for your changes. Lines starting

---
+++ redux-slider-monitor/redux-slider-monitor.d.ts
@@ -0,0 +1,9 @@
+declare module "redux-slider-monitor" {
+    import * as React from "react"
+
+    interface ISliderMonitorProps {
+        // TODO add props
+    }
+
+    export default class SliderMonitor extends React.Component<ISliderMonitorProps, any> {}
+}
