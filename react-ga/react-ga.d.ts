2bc469bf42de156203efb5ca9af445309c60c84e
Omar Hyjek <ohyjek@gmail.com>
2016-12-22 12:00:52 -0500

    last fix I swear…

--- react-ga/react-ga.d.ts
+++ react-ga/react-ga.d.ts
@@ -1,4 +1,5 @@
 declare namespace __reactGA {
+
     export interface EventArgs {
         category: string;
         action: string;
@@ -9,18 +10,24 @@ declare namespace __reactGA {
 
     export interface InitializeOptions {
         debug?: boolean;
-        gaOptions?: gaOptions;
+        gaOptions?: GaOptions;
     }
 
-    export interface gaOptions {
-        userId?: string;
-        cookieDomain?: string;
+    //These are for initialize options
+    export interface GaOptions {
+        [i: string]: string;
+    }
+
+    //These are for set options
+    export interface FieldsObject {
+        [i: string]: string;
     }
 
     export function initialize(trackingCode: string, options?: InitializeOptions): void;
     export function pageview(path: string): void;
     export function modalview(name: string): void;
     export function event(args: EventArgs): void;
+    export function set(fieldsObject: FieldsObject)
 }
 
 declare module 'react-ga' {
