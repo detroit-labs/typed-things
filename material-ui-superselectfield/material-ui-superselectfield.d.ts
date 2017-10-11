0a46baa9bf48af83f288fe8ac07a4ec263ddb985
Mariah Hanson <mariah.hanson@detroitlabs.com>
2017-10-11 15:20:12 -0400

    LNG-2103 Updating typings for superSelectField

--- material-ui-superselectfield/material-ui-superselectfield.d.ts
+++ material-ui-superselectfield/material-ui-superselectfield.d.ts
@@ -2,20 +2,43 @@ declare module "material-ui-superselectfield" {
     import * as React from "react"
 
     interface IMaterialUISuperSelectFieldProps {
-        multiple: boolean;
-        key: string;
         name: string;
-        onChange: (selectedValues: any[], name: string) => void;
-        value: object[];
-        selectionsRenderer: (values: any[]) => void;
-        checkPosition: string;
-        hintText: string;
-        nb2show: number;
-        hoverColor: string;
-        elementHeight: number;
-        style: { marginBottom: number };
-        innerDivStyle: { padding: string, whiteSpace: string };
-        selectedMenuItemStyle: { color: string };
+        value: object[] | object | null;
+        multiple?: boolean;
+        key?: string;
+        onChange?: (...args: any[]) => void;
+        selectionsRenderer?: (...args: any[]) => void;
+        checkPosition?: string;
+        hintText?: string;
+        nb2show?: number;
+        elementHeight?: number;
+        style?: Object;
+        innerDivStyle?: Object;
+        selectedMenuItemStyle?: Object;
+        hoverColor?: string;
+        floatingLabel?: any;
+        hintTextAutocomplete?: string;
+        noMatchFound?: string;
+        anchorOrigin?: { vertical: string, horizontal: string };
+        canAutoPosition?: boolean;
+        disabled?: boolean;
+        onAutoCompleteTyping?: (...args: any[]) => void;
+        children?: any;
+        showAutocompleteThreshold?: number;
+        autocompleteFilter?: (...args: any[]) => void;
+        useLayerForClickAway?: boolean;
+        menuStyle?: Object;
+        menuGroupStyle?: Object;
+        menuFooterStyle?: Object;
+        menuCloseButton?: any;
+        checkedIcon?: any;
+        unCheckedIcon?: any;
+        floatingLabelStyle?: Object;
+        floatingLabelFocusStyle?: Object;
+        underlineStyle?: Object;
+        underlineFocusStyle?: Object;
+        autocompleteUnderlineStyle?: Object;
+        autocompleteUnderlineFocusStyle?: Object;
     }
 
     export default class SuperSelectField extends React.Component<IMaterialUISuperSelectFieldProps, any> {}
